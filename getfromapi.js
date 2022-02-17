f76d954bd0b22e79f3ed2a76cc9dcdefc0f35219
Jonatan Liljeblad <jonatan.liljeblad@live.com>
2022-02-17 13:47:38 +0100

    initial commit

---
+++ getfromapi.js
@@ -0,0 +1,29 @@
+fetch("https://api.opendota.com/api/heroes")
+  .then((response) => response.json())
+  .then((data) => {
+    console.dir(data);
+
+    //let heroes = data.services;
+    //console.log(heroes);
+
+    for (let index = 0; index < data.length; index++) {
+      const element = data[index];
+      let heroName = document.createElement("a");
+      heroName.classList.add("hero-name");
+      heroName.innerHTML = element.localized_name;
+      document.querySelector(".hero_name").append(heroName);
+    }
+
+    /*
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i];
+
+      let div = document.createElement("div");
+      div.innerHTML = `
+            <span>${item.name}</span>
+            <span>${item.status}</span>
+        `;
+      document.querySelector("#root").appendChild(div);
+    }
+    */
+  });
