a31e62c3825126a20ab739d20bc08e5b0529528d
Jonatan Liljeblad <jonatan.liljeblad@live.com>
2022-03-16 09:46:08 +0100

    safe

--- stats.js
+++ stats.js
@@ -3,33 +3,39 @@ fetch("https://api.opendota.com/api/status")
   .then((data) => {
     console.dir(data);
 
-    let mySpan = document.createElement("span");
-    mySpan.classList.add("text");
+    let matchspan = document.createElement("span");
+    matchspan.classList.add("text");
+    let playerspan = document.createElement("span");
+    playerspan.classList.add("text");
+    let hourspan = document.createElement("span");
+    hourspan.classList.add("text");
+    let errorspan = document.createElement("span");
+    errorspan.classList.add("text");
+    let apispan = document.createElement("span");
+    apispan.classList.add("text");
 
     let numberOfPlayers = document.createElement("h1");
     numberOfPlayers.innerHTML = data.user_players;
-    document.querySelector(".numberofplayers").append(numberOfPlayers);
+    playerspan.append(numberOfPlayers);
+    document.querySelector(".numberofplayers").append(playerspan);
 
     let numberOfMatchesLastDay = document.createElement("h1");
     numberOfMatchesLastDay.innerHTML = data.matches_last_day;
-    mySpan.append(numberOfMatchesLastDay);
-    document.querySelector(".numberofmatches").append(mySpan);
+    matchspan.append(numberOfMatchesLastDay);
+    document.querySelector(".numberofmatcheslastday").append(matchspan);
 
     let numberOfMatchesLastHour = document.createElement("h1");
     numberOfMatchesLastHour.innerHTML = data.matches_last_hour;
-    document
-      .querySelector(".numberofmatcheslasthour")
-      .append(numberOfMatchesLastHour);
+    hourspan.append(numberOfMatchesLastHour);
+    document.querySelector(".numberofmatcheslasthour").append(hourspan);
 
     let numberOfErrorsLastDay = document.createElement("h1");
     numberOfErrorsLastDay.innerHTML = data.error_last_day;
-    document
-      .querySelector(".numberoferrorslastday")
-      .append(numberOfErrorsLastDay);
+    errorspan.append(numberOfErrorsLastDay);
+    document.querySelector(".numberoferrorslastday").append(errorspan);
 
     let numberOfApiHitsLastDay = document.createElement("h1");
     numberOfApiHitsLastDay.innerHTML = data.api_hits_last_day;
-    document
-      .querySelector(".numberofapihitslastday")
-      .append(numberOfApiHitsLastDay);
+    apispan.append(numberOfApiHitsLastDay);
+    document.querySelector(".numberofapihitslastday").append(apispan);
   });
